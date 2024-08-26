8d5d2b96b2d59abd2c7202703beda06df8d4829a
Alexandra Putanu <alexandra_putanu@yahoo.com>
2024-08-26 22:08:58 +0300

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: "/goit-react-hw-01-components/", // numele repo-ului tău GitHub
-  plugins: [react()],
-});
